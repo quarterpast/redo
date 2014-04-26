73926fec7fcbcb72d4dd5f8120f8b230c56a2b23
Matt Brennan <mattyb1000@gmail.com>
2014-04-26 10:34:20 +0100

    add apenwarr/redo as submodule, point to included binaries

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var spawn = require('child_process').spawn;
 
 function redo(deps, cb) {
-  spawn('redo', deps, {stdio: 'inherit'}).on('close', function(code) {
+  spawn('./bin/redo', deps, {stdio: 'inherit'}).on('close', function(code) {
     if(code !== 0) return process.exit(code);
     cb();
   });
 }
 
 function redoIfChange(deps, cb) {
-  spawn('redo-ifchange', deps, {stdio: 'inherit'}).on('close', function(code) {
+  spawn('./bin/redo-ifchange', deps, {stdio: 'inherit'}).on('close', function(code) {
     if(code !== 0) return process.exit(code);
     cb();
   });
